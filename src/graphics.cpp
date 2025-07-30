03c815137234d586671ff4c46693cdf246f6dfdf
pedroagaeme <phmss@cin.ufpe.br>
2025-07-30 09:18:10 -0300

    adição de desenho da segunda reta

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -8,7 +8,7 @@
 
 void mouseClickCallback(int button, int state, int mouseX, int mouseY) {
     if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        if(collectedPoints < 3) {
+        if(collectedPoints < 6) {
             collectedPoints++;
         }
         glutPostRedisplay();
@@ -18,9 +18,11 @@ void mouseClickCallback(int button, int state, int mouseX, int mouseY) {
 void passiveMouseMotion(int x, int y) {
     if(collectedPoints < 6) {
         mouseToWorldCoords(x, y, worldX, worldY);
-        double distanceX = worldX - offsetCircle1X;
-        double distanceY = worldY - offsetCircle1Y;
+        double distanceX = worldX;
+        double distanceY = worldY;
         if(collectedPoints < 2) {
+            distanceX -= offsetCircle1X;
+            distanceY -= offsetCircle1Y;
             capDistance2D(distanceX, distanceY);
             markedPoints[collectedPoints] = std::make_tuple(
                 distanceX,
@@ -29,8 +31,32 @@ void passiveMouseMotion(int x, int y) {
                 offsetCircle1Y
             );
         }
-        else if(collectedPoints == 2) {
-            auto [zRotationAngle, direction] = lineBaseRotations[0];
+        else if(collectedPoints > 2 && collectedPoints < 5) {
+            distanceX -= offsetCircle2X;
+            distanceY -= offsetCircle2Y;
+            capDistance2D(distanceX, distanceY);
+            markedPoints[collectedPoints] = std::make_tuple(
+                distanceX,
+                distanceY,
+                offsetCircle2X,
+                offsetCircle2Y
+            );
+        }
+        else {
+            int lineNumber = (collectedPoints + 1) / 3 - 1;
+            int offsetX, offsetY;
+            offsetX = offsetY = 0;
+            if(lineNumber == 0) {
+                offsetX = offsetCircle1X;
+                offsetY = offsetCircle1Y;
+            }
+            else {
+                offsetX = offsetCircle2X;
+                offsetY = offsetCircle2Y;
+            }
+            distanceX -= offsetX;
+            distanceY -= offsetY;
+            auto [zRotationAngle, direction] = lineBaseRotations[lineNumber];
             Vector3 lineLocalDistances = Matrix3::rotationZCos(zRotationAngle, !direction) * Vector3(distanceX, distanceY, 0);
             distanceX = lineLocalDistances[0];
             distanceY = lineLocalDistances[1];
@@ -41,19 +67,20 @@ void passiveMouseMotion(int x, int y) {
                 distanceX = -circleRadius;
             }
             distanceY = sqrt(pow(circleRadius, 2) - pow(distanceX, 2));
-            Vector3 pointInLine = lineTransformations[0] * Vector3(distanceX, distanceY, 0);
+            Vector3 pointInLine = lineTransformations[lineNumber] * Vector3(distanceX, distanceY, 0);
 
             markedPoints[collectedPoints] = std::make_tuple(
                 pointInLine[0],
                 pointInLine[1],
-                offsetCircle1X,
-                offsetCircle1Y
+                offsetX,
+                offsetY
             );
         }
         drawablePoints = collectedPoints + 1;
         glutPostRedisplay();
     }
 }
+
 
 std::tuple<long double, bool, long double> calculateRotations(std::tuple<Vector3, Vector3> line) {
     auto [p1, p2] = line;
@@ -94,14 +121,14 @@ void display(void) {
         auto[px, py, offsetCircleX, offsetCircleY] = markedPoints[j];
         if((pow(circleRadius, 2) - pow(px, 2) - pow(py, 2)) < 0.5) {
             for(float i = 0; i < 2 * M_PI; i += 0.001) {
-                int pxCircle = (5 * cos(i)) - px + offsetCircleX;
-                int pyCircle = (5 * sin(i)) - py + offsetCircleY;
+                int pxCircle = (7 * cos(i)) - px + offsetCircleX;
+                int pyCircle = (7 * sin(i)) - py + offsetCircleY;
             glVertex2i(pxCircle, pyCircle);
         }
         }
         for(float i = 0; i < 2 * M_PI; i += 0.001) {
-            int pxCircle = (5 * cos(i)) + px + offsetCircleX;
-            int pyCircle = (5 * sin(i)) + py + offsetCircleY;
+            int pxCircle = (7 * cos(i)) + px + offsetCircleX;
+            int pyCircle = (7 * sin(i)) + py + offsetCircleY;
             glVertex2i(pxCircle, pyCircle);
         }
     }
@@ -143,6 +170,38 @@ void display(void) {
         x = (circleRadius * cos(i)) + offsetCircle2X;
         y = (circleRadius * sin(i)) + offsetCircle2Y;
         glVertex2i(x, y);
+    }
+
+    // draw line 1 projected onto first circle
+    if(collectedPoints >= 5) {
+        glColor3f(1.0, 1.0, 1.0);
+
+        Vector3 points[2];
+        for(int j = 3; j < 5; j++) {
+            auto [xCoord, yCoord, offsetCircleX, offsetCircleY] = markedPoints[j];
+            double dx = (xCoord);
+            double dy = (yCoord);
+            double norm = pow(circleRadius, 2) - pow(dx, 2) - pow(dy, 2);
+            long double dz = 0;
+            if(norm > 0.5) {
+                dz = sqrt(norm);
+            }
+            points[j - 3] = Vector3(dx, dy, dz);
+        }
+
+        // draw line 2 projected onto second circle
+        auto [zRotationAngle, clockwise, xRotationAngle] = calculateRotations({points[0], points[1]});
+        lineTransformations[1] = Matrix3::rotationZCos(zRotationAngle, clockwise) * Matrix3::rotationXSin(xRotationAngle);
+        lineBaseRotations[1] = std::make_tuple(zRotationAngle, clockwise);
+
+        for(float i = 0; i < M_PI; i += 0.001) {
+            localCoordPoint = Vector3(cos(i), sin(i), 0);
+            globalCoordPoint = lineTransformations[1] * localCoordPoint;
+
+            x = (circleRadius * globalCoordPoint[0]) + offsetCircle2X;
+            y = (circleRadius * globalCoordPoint[1]) + offsetCircle2Y;
+            glVertex2i(x, y);
+        }
     }
 
     glEnd();
